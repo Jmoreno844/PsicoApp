36641341b30f85c044d638621b8fc16439c88d7e
JuanMoreno <jmoreno844@unab.edu.co>
2023-10-30 13:54:12 -0500

    Configurando el blade.php de psicologos

--- resources/views/layout/main.blade.php
+++ resources/views/layout/main.blade.php
@@ -52,11 +52,7 @@
     </div>
   </div>
 </nav>
-<section>
-<h1>Computador</h1>
-<h1>Precio: $100.000</h1>
-<img src="https://www.lifewire.com/thmb/WZdlwJIGSRA9Bkk7tKIj9Mgg-lk=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/xxl-desktop-pc-98013994-5c4dcc47c9e77c0001380389.jpg" class="img-fluid" alt="...">
-</section>
+@yield('content')
 
 </body>
 </html>
---
+++ resources/views/psicologos.blade.php
@@ -0,0 +1,9 @@
+@extends('layout.main')
+@section('content')
+<section>
+<h1>Computador</h1>
+<h1>Precio: $100.000</h1>
+<img src="https://www.lifewire.com/thmb/WZdlwJIGSRA9Bkk7tKIj9Mgg-lk=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/xxl-desktop-pc-98013994-5c4dcc47c9e77c0001380389.jpg" class="img-fluid" alt="...">
+</section>
+{{-- aca se agregaran los psicologos importados de la base de datos y se  pasaran por un for each --}}
+@endsection
